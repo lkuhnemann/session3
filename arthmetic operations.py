9265370ddacfa35e6ff7641aaf7130ed2febcb29
larakuehnemann <lkuhnemann.ieu2023@student.ie.edu>
2025-01-17 13:21:06 +0100

    added 2 more files

---
+++ arthmetic operations.py
@@ -0,0 +1,19 @@
+print(7+11)
+print(7-11)
+print(7*11)
+print(type(9/3), 9/3)
+print(None)
+print(2**10)
+print(8//2)
+print(9%4)
+print(8%9)
+print(44%9)
+
+print(not 7)
+print(not 8)
+
+print(1 or 2 or 3)
+print(1 and 2 and 3)
+
+print(0 or False or 11 or None)
+print(1 and 2 and False and 9)
